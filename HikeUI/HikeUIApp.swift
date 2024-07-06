a904d8ab0403cc591bbf4e528652eb413a302263
Anup Saud <anupsaud10@gmail.com>
2024-07-06 07:35:49 -0400

    Initial Commit

---
+++ HikeUI/HikeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeUIApp.swift
+//  HikeUI
+//
+//  Created by Anup Saud on 2024-07-06.
+//
+
+import SwiftUI
+
+@main
+struct HikeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
